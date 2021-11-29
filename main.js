da36fa6ddcdf0571524df8a3fea8e0d8f4cdadd7
Sarah Franke <sarah.franke@hs-furtwangen.de>
2021-11-29 15:26:28 +0100

    drawing and clear works

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 "use strict";
 window.addEventListener("load", handleLoad);
 let currentColor;
+let canvas;
 let crc2;
 let painting;
+let redBtn;
+let greenBtn;
+let blueBtn;
+let clearBtn;
 function handleLoad(_event) {
-    let canvas = document.querySelector("canvas");
+    canvas = document.querySelector("canvas");
     if (!canvas)
         return;
     crc2 = canvas.getContext("2d");
-    let redBtn = document.querySelector("#redBtn");
-    currentColor = document.getElementsByTagName("div")[1].getAttribute("id");
-    redBtn.addEventListener("click", sarah);
+    redBtn = document.querySelector("#red");
+    greenBtn = document.querySelector("#green");
+    blueBtn = document.querySelector("#blue");
+    clearBtn = document.querySelector("button");
+    redBtn.addEventListener("click", getColor);
+    greenBtn.addEventListener("click", getColor);
+    blueBtn.addEventListener("click", getColor);
+    clearBtn.addEventListener("click", clearCanvas);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mousemove", draw);
     console.log("Hallo Welt");
 }
-function sarah(_event) {
-    console.log("Rot");
-    // let colorType: EventTarget | null = <EventTarget>_event.target;    
-    console.log(currentColor, "canvas");
-    if (currentColor == "redBtn") {
-        console.log("Rot ausgewählt");
-    }
+function getColor(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    console.log(colorType.id);
 }
 function startPainting(_event) {
     painting = true;
@@ -37,12 +44,27 @@ function draw(_event) {
     if (!painting)
         return;
     {
-        crc2.lineWidth = 10;
+        crc2.lineWidth = 6;
         crc2.lineCap = "round";
         crc2.lineTo(_event.clientX, _event.clientY);
         crc2.stroke();
         crc2.beginPath();
         crc2.moveTo(_event.clientX, _event.clientY);
+        switch (currentColor) {
+            case "red":
+                crc2.strokeStyle = "red";
+                break;
+            case "green":
+                crc2.strokeStyle = "green";
+                break;
+            case "blue":
+                crc2.strokeStyle = "blue";
+                break;
+        }
     }
+}
+function clearCanvas(_event) {
+    crc2.fillStyle = "rgb(253, 238, 215)";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
 }
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,37 +1,38 @@
 window.addEventListener("load", handleLoad);
-let currentColor: string | null;
+let currentColor: string;
+let canvas: HTMLCanvasElement | null;
 let crc2: CanvasRenderingContext2D;
 let painting: boolean;
+let redBtn: HTMLDivElement;
+let greenBtn: HTMLDivElement;
+let blueBtn: HTMLDivElement;
+let clearBtn: HTMLButtonElement;
 
 function handleLoad(_event: Event): void {
-    let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+    canvas = document.querySelector("canvas");
     if (!canvas)
         return;
     crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
+    redBtn = <HTMLDivElement>document.querySelector("#red");
+    greenBtn = <HTMLDivElement>document.querySelector("#green");
+    blueBtn = <HTMLDivElement>document.querySelector("#blue");
+    clearBtn = <HTMLButtonElement>document.querySelector("button");
 
+    redBtn.addEventListener("click", getColor);
+    greenBtn.addEventListener("click", getColor);
+    blueBtn.addEventListener("click", getColor);
+    clearBtn.addEventListener("click", clearCanvas);
 
-    let redBtn: HTMLDivElement = <HTMLDivElement>document.querySelector("#redBtn");
-    currentColor = document.getElementsByTagName("div")[1].getAttribute("id");
-    redBtn.addEventListener("click", sarah);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mousemove", draw);
     console.log("Hallo Welt");
 }
 
-function sarah(_event: Event): void {
-
-    console.log("Rot");
-    // let colorType: EventTarget | null = <EventTarget>_event.target;    
-
-
-    console.log(currentColor, "canvas");
-
-    if (currentColor == "redBtn") {
-        console.log("Rot ausgewählt");
-
-    }
-
+function getColor(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+    console.log(colorType.id);
 }
 
 function startPainting(_event: MouseEvent): void {
@@ -48,11 +49,29 @@ function stopPainting(_event: MouseEvent): void {
 function draw(_event: MouseEvent): void {
     if (!painting) return; {
 
-        crc2.lineWidth = 10;
+        crc2.lineWidth = 6;
         crc2.lineCap = "round";
         crc2.lineTo(_event.clientX, _event.clientY);
         crc2.stroke();
         crc2.beginPath();
         crc2.moveTo(_event.clientX, _event.clientY);
+
+        switch (currentColor) {
+            case "red":
+                crc2.strokeStyle = "red";
+                break;
+            case "green":
+                crc2.strokeStyle = "green";
+                break;
+            case "blue":
+                crc2.strokeStyle = "blue";
+                break;
+        }
     }
+}
+
+function clearCanvas(_event: Event): void {
+    crc2.fillStyle = "rgb(253, 238, 215)";
+    crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);  
+
 }
