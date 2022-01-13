0afa17055dc99af6df8bcead3ad05c5da34e2f03
Sarah Franke <sarah.franke@hs-furtwangen.de>
2022-01-13 11:34:52 +0100

    passt soweit

--- main.js
+++ main.js
@@ -7,11 +7,16 @@ let painting;
 let redBtn;
 let greenBtn;
 let blueBtn;
+let yellowBtn;
+let orangeBtn;
+let blackBtn;
+let purpleBtn;
 let clearBtn;
+let okBtn;
 let sound;
 let audio;
-let redactiv = false;
-let blueactiv = false;
+let redactiv;
+let blueactiv;
 let audioToPlay = [];
 let audioLib = ["mdm-owl_1.wav",
     "mdm-owl_2.wav",
@@ -35,68 +40,25 @@ function handleLoad(_event) {
     redBtn = document.querySelector("#red");
     greenBtn = document.querySelector("#green");
     blueBtn = document.querySelector("#blue");
-    clearBtn = document.querySelector("button");
+    yellowBtn = document.querySelector("#yellow");
+    orangeBtn = document.querySelector("#orange");
+    blackBtn = document.querySelector("#black");
+    purpleBtn = document.querySelector("#purple");
+    okBtn = document.querySelector("#ok");
+    clearBtn = document.querySelector("#clear");
     redBtn.addEventListener("click", red);
-    greenBtn.addEventListener("click", getColor);
-    // blueBtn.addEventListener("click", getColor);
+    greenBtn.addEventListener("click", green);
     blueBtn.addEventListener("click", blue);
+    yellowBtn.addEventListener("click", yellow);
+    orangeBtn.addEventListener("click", orange);
+    blackBtn.addEventListener("click", orange);
+    purpleBtn.addEventListener("click", purple);
     clearBtn.addEventListener("click", clearCanvas);
+    okBtn.addEventListener("click", buttonS);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mousemove", draw);
     console.log("Hallo Welt");
-}
-function getColor(_event) {
-    let colorType = _event.target;
-    currentColor = colorType.id;
-    console.log(colorType.id);
-}
-function draw(_event) {
-    if (!painting)
-        return;
-    {
-        crc2.lineWidth = 6;
-        crc2.lineCap = "round";
-        crc2.lineTo(_event.clientX, _event.clientY);
-        crc2.stroke();
-        crc2.beginPath();
-        crc2.moveTo(_event.clientX, _event.clientY);
-        switch (currentColor) {
-            case "red":
-                crc2.strokeStyle = "red";
-                break;
-            case "green":
-                crc2.strokeStyle = "green";
-                break;
-            case "blue":
-                crc2.strokeStyle = "blue";
-                break;
-        }
-        sound.play();
-    }
-}
-function blue(_event) {
-    let colorType = _event.target;
-    currentColor = colorType.id;
-    redactiv = false;
-    blueactiv = true;
-    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
-    let audio = audioLib[indexNumber];
-    let status = audioToPlay.includes(audio);
-    console.log(status);
-    if (status == true) {
-        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
-        let audio = audioLib[indexNumber];
-        sound = new Audio("assets/" + audio);
-        // audioToPlay.push(audio);
-        console.log("ArrayToPlay: ", audioToPlay);
-    }
-    else {
-        sound = new Audio("assets/" + audio);
-        audioToPlay.push(audio);
-        console.log("ArrayToPlay: ", audioToPlay);
-    }
-    console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
 }
 function red(_event) {
     let colorType = _event.target;
@@ -121,9 +83,182 @@ function red(_event) {
     }
     console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
 }
+function green(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    redactiv = true;
+    blueactiv = false;
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    console.log(status);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        // audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    // if (blueactiv == false) {
+    //     backgroundMusic(sound);
+    // }
+}
+function blue(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    redactiv = false;
+    blueactiv = true;
+    // sarah(redactiv);
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    console.log(status);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    // if (redactiv == false) {
+    //     backgroundMusic(sound);
+    // }
+    console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
+}
+function yellow(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+function orange(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+function black(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+function purple(_event) {
+    let colorType = _event.target;
+    currentColor = colorType.id;
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio = audioLib[indexNumber];
+    let status = audioToPlay.includes(audio);
+    if (status == true) {
+        let indexNumber = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+function draw(_event) {
+    if (!painting)
+        return;
+    {
+        crc2.lineWidth = 6;
+        crc2.lineCap = "round";
+        crc2.lineTo(_event.clientX, _event.clientY);
+        crc2.stroke();
+        crc2.beginPath();
+        crc2.moveTo(_event.clientX, _event.clientY);
+        switch (currentColor) {
+            case "red":
+                crc2.strokeStyle = "red";
+                break;
+            case "green":
+                crc2.strokeStyle = "green";
+                break;
+            case "blue":
+                crc2.strokeStyle = "blue";
+                break;
+            case "yellow":
+                crc2.strokeStyle = "yellow";
+                break;
+            case "orange":
+                crc2.strokeStyle = "orange";
+                break;
+            case "black":
+                crc2.strokeStyle = "black";
+                break;
+            case "purple":
+                crc2.strokeStyle = "purple";
+                break;
+        }
+        sound.play();
+    }
+}
+// function sarah(_redactiv: boolean): void {
+//     if (_redactiv == false) {
+//         alert("Hallo Sarah");
+//     }
+// }
 function clearCanvas(_event) {
     crc2.fillStyle = "rgb(253, 238, 215)";
     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    sound.pause();
 }
 function startPainting(_event) {
     painting = true;
@@ -135,8 +270,11 @@ function stopPainting(_event) {
     sound.pause();
     console.log("Stop Painting");
 }
-// function backgroundMusic(_sound: HTMLAudioElement): void {
-//     _sound.play();
-//     console.log("HintergrundLied:", _sound);
-// }
+function buttonS(_event) {
+    backgroundMusic(sound);
+}
+function backgroundMusic(_sound) {
+    _sound.play();
+    console.log("HintergrundLied:", _sound);
+}
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -6,11 +6,16 @@ let painting: boolean;
 let redBtn: HTMLDivElement;
 let greenBtn: HTMLDivElement;
 let blueBtn: HTMLDivElement;
+let yellowBtn: HTMLDivElement;
+let orangeBtn: HTMLDivElement;
+let blackBtn: HTMLDivElement;
+let purpleBtn: HTMLDivElement;
 let clearBtn: HTMLButtonElement;
+let okBtn: HTMLButtonElement;
 let sound: HTMLAudioElement;
 let audio: string;
-let redactiv: boolean = false;
-let blueactiv: boolean = false;
+let redactiv: boolean;
+let blueactiv: boolean;
 let audioToPlay: string[] = [];
 let audioLib: string[] = ["mdm-owl_1.wav",
     "mdm-owl_2.wav",
@@ -36,81 +41,27 @@ function handleLoad(_event: Event): void {
     redBtn = <HTMLDivElement>document.querySelector("#red");
     greenBtn = <HTMLDivElement>document.querySelector("#green");
     blueBtn = <HTMLDivElement>document.querySelector("#blue");
-    clearBtn = <HTMLButtonElement>document.querySelector("button");
+    yellowBtn = <HTMLDivElement>document.querySelector("#yellow");
+    orangeBtn = <HTMLDivElement>document.querySelector("#orange");
+    blackBtn = <HTMLDivElement>document.querySelector("#black");
+    purpleBtn = <HTMLDivElement>document.querySelector("#purple");
+    okBtn = <HTMLButtonElement>document.querySelector("#ok");
+    clearBtn = <HTMLButtonElement>document.querySelector("#clear");
 
     redBtn.addEventListener("click", red);
-    greenBtn.addEventListener("click", getColor);
-    // blueBtn.addEventListener("click", getColor);
+    greenBtn.addEventListener("click", green);
     blueBtn.addEventListener("click", blue);
+    yellowBtn.addEventListener("click", yellow);
+    orangeBtn.addEventListener("click", orange);
+    blackBtn.addEventListener("click", orange);
+    purpleBtn.addEventListener("click", purple);
     clearBtn.addEventListener("click", clearCanvas);
+    okBtn.addEventListener("click", buttonS);
 
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mousemove", draw);
     console.log("Hallo Welt");
-
-}
-
-function getColor(_event: Event): void {
-    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
-    currentColor = colorType.id;
-    console.log(colorType.id);
-}
-
-function draw(_event: MouseEvent): void {
-    if (!painting) return; {
-        crc2.lineWidth = 6;
-        crc2.lineCap = "round";
-        crc2.lineTo(_event.clientX, _event.clientY);
-        crc2.stroke();
-        crc2.beginPath();
-        crc2.moveTo(_event.clientX, _event.clientY);
-        switch (currentColor) {
-            case "red":
-                crc2.strokeStyle = "red";
-                break;
-            case "green":
-                crc2.strokeStyle = "green";
-                break;
-            case "blue":
-                crc2.strokeStyle = "blue";
-                break;
-        }
-        sound.play();
-    }
-
-}
-
-function blue(_event: Event): void {
-
-  
-    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
-    currentColor = colorType.id;
-
-    redactiv = false;
-    blueactiv = true;
-
-
-    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
-    let audio: string = audioLib[indexNumber];
-
-    let status: boolean = audioToPlay.includes(audio);
-
-    console.log(status);
-
-    if (status == true) {
-        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
-        let audio: string = audioLib[indexNumber];
-        sound = new Audio("assets/" + audio);
-        // audioToPlay.push(audio);
-        console.log("ArrayToPlay: ", audioToPlay);
-    } else {
-        sound = new Audio("assets/" + audio);
-        audioToPlay.push(audio);
-        console.log("ArrayToPlay: ", audioToPlay);
-    }
-    console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
-
 
 }
 
@@ -142,14 +93,221 @@ function red(_event: Event): void {
         console.log("ArrayToPlay: ", audioToPlay);
     }
     console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
+}
 
-   
+function green(_event: Event): void {
+
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    redactiv = true;
+    blueactiv = false;
+
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    console.log(status);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        // audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    // if (blueactiv == false) {
+    //     backgroundMusic(sound);
+    // }
+}
+
+function blue(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    redactiv = false;
+    blueactiv = true;
+    // sarah(redactiv);
+
+
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    console.log(status);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+    // if (redactiv == false) {
+    //     backgroundMusic(sound);
+    // }
+    console.log("Status rot: ", redactiv, "Status blau: ", blueactiv);
+}
+
+function yellow(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+
+function orange(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+
+
+function black(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
+}
+
+function purple(_event: Event): void {
+    let colorType: HTMLDivElement = <HTMLDivElement>_event.target;
+    currentColor = colorType.id;
+
+    // // redactiv = true;
+    // // blueactiv = false;
+    let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und 3
+    let audio: string = audioLib[indexNumber];
+
+    let status: boolean = audioToPlay.includes(audio);
+
+    if (status == true) {
+        let indexNumber: number = Math.floor((Math.random() * 13) + 0); // eine zufällige Zahl zwischen 0 und x
+        let audio: string = audioLib[indexNumber];
+        sound = new Audio("assets/" + audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    } else {
+        sound = new Audio("assets/" + audio);
+        audioToPlay.push(audio);
+        console.log("ArrayToPlay: ", audioToPlay);
+    }
 
 }
+
+
+
+function draw(_event: MouseEvent): void {
+    if (!painting) return; {
+        crc2.lineWidth = 6;
+        crc2.lineCap = "round";
+        crc2.lineTo(_event.clientX, _event.clientY);
+        crc2.stroke();
+        crc2.beginPath();
+        crc2.moveTo(_event.clientX, _event.clientY);
+        switch (currentColor) {
+            case "red":
+                crc2.strokeStyle = "red";
+                break;
+            case "green":
+                crc2.strokeStyle = "green";
+                break;
+            case "blue":
+                crc2.strokeStyle = "blue";
+                break;
+            case "yellow":
+                crc2.strokeStyle = "yellow";
+                break;
+            case "orange":
+                crc2.strokeStyle = "orange";
+                break;
+            case "black":
+                crc2.strokeStyle = "black";
+                break;
+            case "purple":
+                crc2.strokeStyle = "purple";
+                break;
+        }
+        sound.play();
+    }
+}
+
+
+
+
+// function sarah(_redactiv: boolean): void {
+//     if (_redactiv == false) {
+//         alert("Hallo Sarah");
+//     }
+
+// }
+
+
+
+
+
 
 function clearCanvas(_event: Event): void {
     crc2.fillStyle = "rgb(253, 238, 215)";
     crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+    sound.pause();
 }
 
 
@@ -158,7 +316,7 @@ function clearCanvas(_event: Event): void {
 function startPainting(_event: MouseEvent): void {
     painting = true;
     console.log("Start Painting");
-    }
+}
 
 function stopPainting(_event: MouseEvent): void {
     painting = false;
@@ -166,10 +324,13 @@ function stopPainting(_event: MouseEvent): void {
     sound.pause();
     console.log("Stop Painting");
 }
+function buttonS(_event: Event): void {
+    backgroundMusic(sound);
 
-// function backgroundMusic(_sound: HTMLAudioElement): void {
+}
+function backgroundMusic(_sound: HTMLAudioElement): void {
 
 
-//     _sound.play();
-//     console.log("HintergrundLied:", _sound);
-// }
+    _sound.play();
+    console.log("HintergrundLied:", _sound);
+}
